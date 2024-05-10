be2fbd6b8fe88ca080ada3fe3f2eb7d3180c4bcc
lvyuma <1043551197@qq.com>
2024-05-10 16:54:55 +0800

    添加Dog.java实体类

---
+++ StoreAdmin/src/main/java/com/example/store/bean/Dog.java
@@ -0,0 +1,15 @@
+package com.example.store.bean;
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
