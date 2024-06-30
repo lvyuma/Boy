a1d8e4107e6095430d9ad0dd4e8c2b4187814be1
lvyuma <1043551197@qq.com>
2024-06-30 14:25:27 +0800

    删除数据

--- StoreAdmin/src/main/java/com/example/store/bean/Dog.java
+++ StoreAdmin/src/main/java/com/example/store/bean/Dog.java
@@ -1,22 +1,6 @@
 package com.example.store.bean;
 
 public class Dog {
-    //t1 1
-    //master 1
-
-    //t2
-    //t3
-
-    //t1  t4
-
-    //t1 t5 1111  abc
-    //t1 t5
-
-    //20240630
-    //20240630 001
-
-    //合并t1到主分支
-    //123
     private String name;
     private int age;
 
