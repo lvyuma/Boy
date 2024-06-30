aee8976e7e628833328a81d0accfe71139930063
lvyuma <59731231+lvyuma@users.noreply.github.com>
2024-06-30 14:19:59 +0800

    Update Dog.java

--- StoreAdmin/src/main/java/com/example/store/bean/Dog.java
+++ StoreAdmin/src/main/java/com/example/store/bean/Dog.java
@@ -1,27 +1,6 @@
 package com.example.store.bean;
 
 public class Dog {
-    //t1 1
-    //master 1
-
-    //t2
-    //t3  chongtu
-
-    //master t4  master12345
-
-    //t1  t4
-
-    //t1 t5 1111
-
-    //主分支已经有
-
-    //t1 t5 1111  abc
-    //t1 t5
-
-    //20240630
-    //20240630 001
-
-    //合并t1到主分支
     private String name;
     private int age;
 
