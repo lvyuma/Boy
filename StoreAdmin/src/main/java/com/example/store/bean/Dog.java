d0be9d3a38405de173f2d38ffeaafbb81e8826cb
lvyuma <1043551197@qq.com>
2024-07-03 23:04:01 +0800

    t2合并到master

---
+++ StoreAdmin/src/main/java/com/example/store/bean/Dog.java
@@ -0,0 +1,27 @@
+package com.example.store.bean;
+
+public class Dog {
+    //master分支
+    //t2
+    //t3
+    //t4
+    //t5
+    //t6
+    //t7
+    //t2
+    private String name;
+    private int age;
+
+    final  int a = 0;
+
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
